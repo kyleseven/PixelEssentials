c48180b7fd1d0966cbaea2d475bc331d3c12e7c5
Kyle Senerpida <46883293+kyleseven@users.noreply.github.com>
2025-02-21 12:39:40 -0600

    Support console in DMs

--- src/main/kotlin/me/kyleseven/pixelessentials/commands/ChatCommands.kt
+++ src/main/kotlin/me/kyleseven/pixelessentials/commands/ChatCommands.kt
@@ -8,6 +8,7 @@ import co.aikar.commands.annotation.Description
 import me.kyleseven.pixelessentials.utils.mmd
 import org.bukkit.Bukkit
 import org.bukkit.command.CommandSender
+import org.bukkit.command.ConsoleCommandSender
 import java.time.Instant
 
 class ChatCommands : BaseCommand() {
@@ -15,46 +16,60 @@ class ChatCommands : BaseCommand() {
 
     private val lastReceivedMessages = hashMapOf<String, MessageData>()
     private val replyTimeout = 300L
+    private val consoleName = Bukkit.getConsoleSender().name
 
     private fun isMessageExpired(messageData: MessageData): Boolean {
         return Instant.now().epochSecond - messageData.timestamp.epochSecond > replyTimeout
     }
 
+    private fun sendPrivateMessage(sender: CommandSender, receiver: CommandSender, message: String) {
+        val senderName = if (sender is ConsoleCommandSender) consoleName else sender.name
+        val receiverName = if (receiver is ConsoleCommandSender) consoleName else receiver.name
+
+        sender.sendMessage(mmd("<gray>[You -> <white>$receiverName</white>]:</gray> $message"))
+        receiver.sendMessage(mmd("<gray>[<white>$senderName</white> -> You]:</gray> $message"))
+
+        val trackingKey = if (receiver is ConsoleCommandSender) consoleName else receiver.name
+        lastReceivedMessages[trackingKey] = MessageData(senderName, Instant.now())
+    }
+
     @CommandAlias("tell|msg|m|whisper")
-    @Description("Send a private message to a player")
-    @CommandCompletion("@players")
+    @Description("Send a private message to a player or console")
+    @CommandCompletion("@players|console")
     @CommandPermission("pixelessentials.tell")
-    fun onTell(sender: CommandSender, playerName: String, message: String) {
-        val otherPlayer = Bukkit.getPlayer(playerName)
-        if (otherPlayer == null) {
-            sender.sendMessage(mmd("<red>Player not found</red>"))
-            return
+    fun onTell(sender: CommandSender, targetName: String, message: String) {
+        val target = when (targetName.lowercase()) {
+            "console" -> Bukkit.getConsoleSender()
+            else -> Bukkit.getPlayer(targetName) ?: run {
+                sender.sendMessage(mmd("<red>Target not found</red>"))
+                return
+            }
         }
 
-        lastReceivedMessages[otherPlayer.name] = MessageData(sender.name, Instant.now())
-
-        sender.sendMessage(mmd("<gray>[You -> <white>${otherPlayer.name}</white>]:</gray> $message"))
-        otherPlayer.sendMessage(mmd("<gray>[<white>${sender.name}</white> -> You]:</gray> $message"))
+        sendPrivateMessage(sender, target, message)
     }
 
     @CommandAlias("reply|r")
     @Description("Reply to the last player who messaged you")
     @CommandPermission("pixelessentials.tell")
     fun onReply(sender: CommandSender, message: String) {
-        val lastMessageData = lastReceivedMessages[sender.name]
+        val trackingKey = if (sender is ConsoleCommandSender) consoleName else sender.name
+        val lastMessageData = lastReceivedMessages[trackingKey]
+
         if (lastMessageData == null || isMessageExpired(lastMessageData)) {
             sender.sendMessage(mmd("<red>Nobody has messaged you recently.</red>"))
-            lastReceivedMessages.remove(sender.name)
+            lastReceivedMessages.remove(trackingKey)
             return
         }
 
-        val otherPlayer = Bukkit.getPlayer(lastMessageData.senderName)
-        if (otherPlayer == null) {
-            sender.sendMessage(mmd("<red>Player not found.</red>"))
-            return
+        val target = when (lastMessageData.senderName) {
+            consoleName -> Bukkit.getConsoleSender()
+            else -> Bukkit.getPlayer(lastMessageData.senderName) ?: run {
+                sender.sendMessage(mmd("<red>Sender is no longer available.</red>"))
+                return
+            }
         }
 
-        sender.sendMessage(mmd("<gray>[You -> <white>${otherPlayer.name}</white>]:</gray> $message"))
-        otherPlayer.sendMessage(mmd("<gray>[<white>${sender.name}</white> -> You]:</gray> $message"))
+        sendPrivateMessage(sender, target, message)
     }
 }
