2e0a66c68cadb3f0ee86b700150f3a7ff7d792f8
kyleseven <46883293+kyleseven@users.noreply.github.com>
2025-02-23 02:45:16 -0600

    Fix invsee commands so they actually work :/

--- src/main/kotlin/me/kyleseven/pixelessentials/commands/AdminCommands.kt
+++ src/main/kotlin/me/kyleseven/pixelessentials/commands/AdminCommands.kt
@@ -5,6 +5,9 @@ import co.aikar.commands.annotation.CommandAlias
 import co.aikar.commands.annotation.CommandCompletion
 import co.aikar.commands.annotation.CommandPermission
 import co.aikar.commands.annotation.Description
+import co.aikar.commands.bukkit.contexts.OnlinePlayer
+import me.kyleseven.pixelessentials.utils.mmd
+import me.kyleseven.pixelessentials.utils.mms
 import org.bukkit.entity.Player
 
 class AdminCommands : BaseCommand() {
@@ -12,15 +15,17 @@ class AdminCommands : BaseCommand() {
     @Description("Open the inventory of another player")
     @CommandCompletion("@players")
     @CommandPermission("pixelessentials.invsee")
-    fun onInvsee(player: Player, target: Player) {
-        player.openInventory(target.inventory)
+    fun onInvsee(player: Player, target: OnlinePlayer) {
+        player.sendMessage(mmd("<gray>Opening <white>${mms(target.player.displayName())}'s</white> inventory.</gray>"))
+        player.openInventory(target.player.inventory)
     }
 
     @CommandAlias("enderchest|ec")
     @Description("Open the ender chest of another player")
     @CommandCompletion("@players")
     @CommandPermission("pixelessentials.enderchest")
-    fun onEnderchest(player: Player, target: Player) {
-        player.openInventory(target.enderChest)
+    fun onEnderchest(player: Player, target: OnlinePlayer) {
+        player.sendMessage(mmd("<gray>Opening <white>${mms(target.player.displayName())}'s</white> ender chest.</gray>"))
+        player.openInventory(target.player.enderChest)
     }
 }
