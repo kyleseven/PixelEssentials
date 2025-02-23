65e04a8f1f059fba13eb6a6e94adb7585f2cd842
Kyle Senerpida <46883293+kyleseven@users.noreply.github.com>
2025-02-23 04:13:35 -0600

    /ec opens your own ender chest if no player is specified

--- src/main/kotlin/me/kyleseven/pixelessentials/commands/AdminCommands.kt
+++ src/main/kotlin/me/kyleseven/pixelessentials/commands/AdminCommands.kt
@@ -1,10 +1,7 @@
 package me.kyleseven.pixelessentials.commands
 
 import co.aikar.commands.BaseCommand
-import co.aikar.commands.annotation.CommandAlias
-import co.aikar.commands.annotation.CommandCompletion
-import co.aikar.commands.annotation.CommandPermission
-import co.aikar.commands.annotation.Description
+import co.aikar.commands.annotation.*
 import co.aikar.commands.bukkit.contexts.OnlinePlayer
 import me.kyleseven.pixelessentials.utils.mmd
 import me.kyleseven.pixelessentials.utils.mms
@@ -24,7 +21,13 @@ class AdminCommands : BaseCommand() {
     @Description("Open the ender chest of another player")
     @CommandCompletion("@players")
     @CommandPermission("pixelessentials.enderchest")
-    fun onEnderchest(player: Player, target: OnlinePlayer) {
+    fun onEnderchest(player: Player, @Optional target: OnlinePlayer?) {
+        if (target == null) {
+            player.sendMessage(mmd("<gray>Opening your ender chest.</gray>"))
+            player.openInventory(player.enderChest)
+            return
+        }
+
         player.sendMessage(mmd("<gray>Opening <white>${mms(target.player.displayName())}'s</white> ender chest.</gray>"))
         player.openInventory(target.player.enderChest)
     }
